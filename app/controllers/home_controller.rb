0d5f2e30b0650f93ca1f1b9dd67497e89b58329f
Julian Arango <julian_arango08@hotmail.com>
2016-12-07 00:02:55 -0500

    Iniciando blog personal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def estudios
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
+  get '/estudios' => 'home#estudios'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
